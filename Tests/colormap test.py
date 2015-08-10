809d38e15e54e31dbb02c5e2fd284ae1148b5ee1
violasox <viola.sox@gmail.com>
2015-08-10 10:33:19 -0700

    Added folder for testing

---
+++ Tests/colormap test.py
@@ -0,0 +1,25 @@
+import tellurium as te
+from tellurium import ParameterScan as ps
+
+
+cell = '''
+    $Xo -> S1; vo;
+    S1 -> S2; k1*S1 - k2*S2;
+    S2 -> $X1; k3*S2;
+    
+    vo = 1
+    k1 = 2; k2 = 0; k3 = 3;
+'''
+
+rr = te.loadAntimonyModel(cell)
+p = ps.ParameterScan(rr)
+
+p.endTime = 3
+p.colormap = p.createColormap([.12,.56,1], [.86,.08,.23])
+p.dependent = ['S1']
+p.independent = ['time', 'k1']
+p.startValue = 0.5
+p.endValue = 5
+p.title = 'Concentration of S1'
+
+p.plotSurface()
